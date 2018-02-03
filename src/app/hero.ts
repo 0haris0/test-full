edb5754b2e3bb04b610f7366ec2782e7e945198e
Haris <haris.valj@gmail.com>
2018-02-03 23:09:54 +0100

    Edited hero.ts class file

--- src/app/hero.ts
+++ src/app/hero.ts
@@ -1,4 +1,41 @@
 export class Hero {
-  id: number;
-  name: string;
-}
+    id: string;
+    type: string;
+    links: {
+      self: string;
+    };
+    attributes: {
+      name: string;
+      slug: string;
+      image_portrait: string;
+      image_splash: string;
+      updated_at: string;
+      image_card_background: string;
+    };
+    relationships: {
+      hero_relationships: {
+        links: {
+          self: string;
+          related: string;
+        }
+      },
+      hero_general_tips: {
+        links: {
+          self: string;
+          related: string;
+        }
+      },
+      hero_stat_percentiles: {
+        links: {
+          self: string;
+          related: string;
+        }
+      },
+      hero_role: {
+        links: {
+          self: string;
+          related: string;
+        }
+      }
+    }
+  }
